3044fdc46fc6f3b1a6d875a15fa627feb4dd952f
denise duffy <denise_duffy@denises-MacBook-Pro.local>
2017-05-04 16:00:46 -0700

    Add seed file

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+
+u = User.create :username => "a", :email => "a@a.com", :password => "a"
+s = School.create :name => "Bwindi Nursery Primary School", :location => "Bwindi National Forrest", :level => "Primary"
+c = Child.create :name => "Barbrah Gongo", :birthdate => DateTime.parse "04-05-1999", :school_id => s.id
+Donation.create :user_id = u.id, :child_id = c.id, :school = s.id 
